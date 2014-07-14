2ea8e8e7af643ff7d8145c4205b0aa1a5cd6d3cc
Matthias Goetzke <m.goetzke@curasystems.de>
2014-07-14 17:27:30 +0200

    use coffee-script/register, DONT use source-maps (which interferes with coffee-script/register)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Support coffee script and source maps for stack traces
 // NOTE: don't handle uncaught exceptions in order not to interfere with mocha.
-require('coffee-script');
-require('source-map-support').install( {handleUncaughtExceptions:false} );
+require('coffee-script/register');
 
 // enable chai should as default for all tests
 module.exports = require('./lib/store');
